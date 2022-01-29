d7dd66504e2de0787151416c67686a698bb7164e
chamibia <b.chami39@gmail.com>
2022-01-28 21:42:56 -0500

    added models

---
+++ controllers/home-routes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+//get all posts for homepage
+router.get("/", (req, res) => {
+  console.log("=============");
+
+  Post.findAll;
+});
